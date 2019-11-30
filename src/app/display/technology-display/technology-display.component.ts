c0410988673dcc896201a4b60bcd05f204260b88
yudi@max.com <yudi@max.com>
2019-11-30 12:46:08 +0530

    Project 30 Nov

---
+++ src/app/display/technology-display/technology-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-technology-display',
+  templateUrl: './technology-display.component.html',
+  styleUrls: ['./technology-display.component.css']
+})
+export class TechnologyDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
